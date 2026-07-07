f53127df375e90b0f3e00b6ec03736a79591b002
Giorgos <giorgos@NM0083s-MacBook-Neo.local>
2026-07-07 10:24:37 +0900

    setup lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+	modules: ["@nuxt/eslint"],
+	devtools: { enabled: true },
+	compatibilityDate: "2025-07-15",
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+});
